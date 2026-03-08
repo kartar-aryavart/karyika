89c3b848a4545d9ae0c3c74e642b998cf6b877ac
kartar-aryavart <sanatanikkartar@gmail.com>
2026-03-09 00:30:26 +0530

    Fix import path for LangProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './hooks/useAuth.jsx'
-import { LangProvider } from './i18n/translations.js'
+import { LangProvider } from './i18n/translations'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
